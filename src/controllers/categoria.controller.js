e379aadf998cc960d925a31199b1189282aa61e1
lucasferrre <lucasferreirasasuke@gmail.com>
2026-06-18 11:13:00 -0300

    alterando criar categoria

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -19,7 +19,7 @@ const categoriaController = {
 
      criarCategoria: async (req, res) => {
         try {
-            const { descricaoCategoria } = req.body.descricaoCategoria;
+            const { descricaoCategoria } = req.body;
 
             if (!descricaoCategoria) {
                 return res.status(400).json({
